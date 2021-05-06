228458022d471baf29998e69fcc5b7df7f5a4bf9
dtd1232 <dtd12321@gmail.com>
2021-05-06 21:28:04 +0900

    add .java files in edu.handong.csee.java.hw2

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,17 @@
+package edu.handong.csee.java.hw2;
+
+/**
+ * class for capital letter
+ */
+public class Util {
+
+    /**
+     * string to capital letter
+     * @param stringValue
+     * @return
+     */
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
